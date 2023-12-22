ccd483dc041f0b1d319ce0994f3a2b58847db287
Anton Kostiuk <ak4932328@gmail.com>
2023-12-22 17:10:46 +0300

    update generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
-
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   server: {
     host: 'localhost',
     port: 80,
   },
-
+  generate: {
+    fallback: 'index.html'
+  },
 
   serverMiddleware: [
     createProxyMiddleware('/api/mos', {
@@ -47,29 +46,8 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
 }
