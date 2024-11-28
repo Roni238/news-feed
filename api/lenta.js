d49f57f8e1f220276dbd1a04ab280f8d0bb2b49c
Anton Kostiuk <ak4932328@gmail.com>
2024-11-28 14:45:31 +0300

    deploy-vercel

---
+++ api/lenta.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (req, res) => {
+  const proxy = createProxyMiddleware({
+    target: 'https://lenta.ru/rss/news',
+    changeOrigin: true,
+    timeout: 60000, // 20 секунд
+    proxyTimeout: 60000, // 20 секунд
+    pathRewrite: { '^/api/lenta': '' },
+  });
+  console.log('запрос')
+  return proxy(req, res, (err) => {
+    if (err) {
+      console.error('Proxy error:', err);
+      res.status(500).send('Proxy error');
+    }
+  });
+};
